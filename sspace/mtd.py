7bda68513916d3f3c02f1239d8cb25fccbf27dd2
Nima Nabizadeh <nima.nabizadeh@rub.de>
2020-02-20 14:38:23 +0100

    MTD is running

--- sspace/mtd.py
+++ sspace/mtd.py
@@ -45,7 +45,8 @@ def initialize():
 
 
 def posterior(k, t1, t2):
-    return (landamat[k] * mmat[k][t1][t2]) / sum([landamat[i] * mmat[i][t1][t2] for i in range(maxorder)])
+    denum = sum([landamat[i] * mmat[i][t1][t2] for i in range(maxorder)])
+    return (landamat[k] * mmat[k][t1][t2]) / denum
 
 
 def estep():
@@ -63,22 +64,32 @@ def mstep(data):
         for seq in data:
             for t in range(k, len(seq)):
                 landasum += phimat[k-1][seq[t]][seq[t - k]]
-                landanorm += sum([phimat[i-1][seq[t]][seq[t - i]] for i in range(1, min(maxorder + 1, len(seq)))])
+                landanorm += sum([phimat[i-1][seq[t]][seq[t - i]] for i in range(1, min(maxorder + 1, len(seq)+1))])
                 msum[seq[t]][seq[t - k]] += phimat[k-1][seq[t]][seq[t - k]]
         landamat[k-1] = landasum / landanorm
 
-        for i in range(len(msum)):
+        for i in range(1,vocablen):
             mnorm = sum(msum[i])
-            for j in range(len(msum[i])):
-                mmat[k-1][i][j] = msum[i][j] / mnorm
+            if mnorm != 0:
+                zeros = vocablen - np.count_nonzero(msum[i])
+                for j in range(vocablen):
+                        if msum[i][j] ==0:
+                            val = alpha/zeros
+                        else:
+                            val =  msum[i][j]-alpha
+                        mmat[k-1][i][j] = val / mnorm
+            else:
+                for j in range(vocablen):
+                    mmat[k - 1][i][j] = 1/vocablen
+
 
 
 def predict(seq):
     summ = np.zeros([vocablen])
     for t in mydata.decodedic:
-        for k in range(1, min(maxorder + 1, len(seq))):
+        for k in range(1, min(maxorder + 1, len(seq)+1)):
             summ[t] += landamat[k-1] * mmat[k-1][t][seq[-k]]
-    return np.argmax(summ)
+    return np.nanargmax(summ)
 
 def test():
     total = correct = 0
@@ -87,7 +98,11 @@ def test():
         for t in seq[1:]:
             total +=1
             p = predict(olds)
-            if p == t: correct+=1
+            if p == t:
+                correct+=1
+            # else:
+            #     print(t,p)
+
             olds.append(t)
     return correct/total
 
@@ -103,15 +118,14 @@ def run_em():
         estep()
         mstep(mydata.train)
         accu = test()
-        print('Accuracy:', accu)
+        print('Accuracy:', accu*100)
     return 0
 
 
 if __name__ == '__main__':
-    END_TOK, START_TOK = 55, 1
     mydata = Data(0, encod=True)
     vocablen = len(mydata.encodedic)
-    maxorder = 5
-    emitr = 10
+    maxorder = 10
+    emitr = 100
+    alpha = 1e-10
     run_em()
-    # test()
