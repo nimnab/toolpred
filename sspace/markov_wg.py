876bd8502d095dc6b995a5dfc6eddbc6449e7f49
Nima Nabizadeh <nima.nabizadeh@rub.de>
2020-02-21 14:17:26 +0100

    tfidf tried

--- sspace/markov_wg.py
+++ sspace/markov_wg.py
@@ -1,36 +1,36 @@
 
 
 from .chain import Chain_withid
-from numpy import average
+from utils.sentemb import cosin_sim , fasttext , tfidf
 from utils.datas import Data
 from utils.util import mean_confidence_interval , ngram_simscore , output, load_obj, save_obj
 import numpy as np
 
 datapath = '/hri/localdisk/nnabizad/toolpreddata/'
 
-simdic = load_obj(datapath + 'ngram-similarities')
+# simdic = load_obj(datapath + 'ngram-similarities')
+simdic = dict()
 
-def similarity_score (id1, ids):
+def similarity_score(id1, ids):
     score = 0
     for id in ids:
         if (' ' .join(mydata.titles_test[id1]),' '.join(mydata.titles_train[id])) not in simdic:
-            sim =  ngram_simscore(mydata.titles_test[id1], mydata.titles_train[id])
+            sim =  tfidf(mydata.titles_test[id1], mydata.titles_train[id])
             simdic[(' ' .join(mydata.titles_test[id1]),' '.join(mydata.titles_train[id]))] = sim
         score += simdic[(' ' .join(mydata.titles_test[id1]),' '.join(mydata.titles_train[id]))]
     # print(score)
     return score/len(ids)
 
 
-
 def predict(lis, id):
     global prediction
     history = tuple(lis)
-    landa = 0.4
+    # landa = 0.5
     order = (maxst-1) if len(lis) > (maxst-1) else len(lis)
     if history in models[order-1].keys():
         normp = sum([models[order-1][history][k][0] for k in models[order-1][history].keys()])
         norms = sum([similarity_score(id,models[order-1][history][k][1]) for k in models[order-1][history].keys()])
-        prediction = max(models[order-1][history].keys(), key=(lambda k: landa * np.log(models[order-1][history][k][0]/normp) + (1-landa)*np.log(similarity_score(id,models[order-1][history][k][1])/norms)))
+        prediction = max(models[order-1][history].keys(), key=(lambda k: landa * np.log(models[order-1][history][k][0]/normp) + (1-landa)*np.log(similarity_score(id,models[order-1][history][k][1]))))
         return prediction
     elif len(lis) > 0:
         lis2 = lis[1:]
@@ -63,7 +63,7 @@ def average_len(l):
 def write_result(filename):
     seeds = [0, 12, 21, 32, 45, 64, 77, 98, 55, 120]
     accu_list = []
-    for n, seed in enumerate(seeds):
+    for n, seed in enumerate(seeds[0:1]):
         global mydata
         mydata = Data(seed, titles=True)
         prediction = 0
@@ -72,13 +72,15 @@ def write_result(filename):
         # maxst = 2
         global models
         models = [Chain_withid(mydata.train, i).model for i in range(1,maxst)]
-        preds , acc = accu_all(mydata.test)
-        accu_list.append(acc)
-        print(acc)
+        global landa
+        for landa in np.arange(0, 1.2, 0.2):
+            preds , acc = accu_all(mydata.test)
+            accu_list.append(acc)
+            print("landa {}, accuracy {}".format(landa, acc))
     output(mean_confidence_interval(accu_list),filename=filename, func='write')
     output(accu_list,filename=filename, func='write')
 
 
 if __name__ == '__main__':
-    write_result('/home/nnabizad/code/toolpred/sspace/res/mac/akom_wig_landa.txt')
-    save_obj(simdic, 'ngram-similarities')
+    write_result('/home/nnabizad/code/toolpred/sspace/res/mac/akom_wig_tfidf.txt')
+    # save_obj(simdic, 'fast-similarities')
--- utils/sentemb.py
+++ utils/sentemb.py
@@ -5,16 +5,35 @@ from utils.datas import Data
 from gensim.models.keyedvectors import KeyedVectors, FastTextKeyedVectors
 from numpy import dot
 from gensim import matutils
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import sister
+
+
+
 
 datapath = '/hri/localdisk/nnabizad/toolpreddata/'
-glove = KeyedVectors.load("/hri/localdisk/nnabizad/w2v/glove100_word2vec1")
-model = uSIF(glove, workers=1, lang_freq="en")
 mydata = Data(0, titles=True)
-sentences = IndexedList(mydata.titles_train)
-model.train(sentences)
-# model.load(datapath + 'usif-model')
 
+
+# glove = KeyedVectors.load("/hri/localdisk/nnabizad/w2v/glove100_word2vec1")
+# model = SIF(glove, workers=1, lang_freq="en")
+# sentences = IndexedList(mydata.titles_train)
+# model.train(sentences)
+#
+# sentence_embedding = sister.MeanEmbedding(lang="en")
+
+
+vectorizer = TfidfVectorizer(analyzer='word', ngram_range=(1,5), min_df = 0, stop_words = 'english')
+X = vectorizer.fit([' '.join(s) for s in mydata.titles_train])
+
+
+def tfidf(text1, text2):
+    vec1 = X.transform([' '.join(text1)])
+    vec2 = X.transform([' '.join(text2)])
+    sim = np.dot(matutils.unitvec(vec1), np.transpose(matutils.unitvec(vec2))).toarray()[0][0]
+    # print("sim", sim)
+    return sim
 
 
 def train(model, sents):
@@ -23,10 +42,20 @@ def train(model, sents):
     model.save(datapath + 'usif-model')
 
 
+def fasttext(text1, text2):
+    vec1 = sentence_embedding(' '.join(text1))
+    vec2 = sentence_embedding(' '.join(text2))
+    sim = np.dot(matutils.unitvec(vec1), np.transpose(matutils.unitvec(vec2)))
+    # print("sim",sim)
+    return sim
+
+
 def cosin_sim(text1, text2):
     vec1 = model.infer([(text1,0)])
     vec2 = model.infer([(text2,0)])
-    return cosine_similarity(matutils.unitvec(vec1), matutils.unitvec(vec2))[0][0]
+    sim = np.dot(matutils.unitvec(vec1), np.transpose(matutils.unitvec(vec2)))
+    # print("sim",sim)
+    return sim
 
 
 if __name__ == '__main__':
